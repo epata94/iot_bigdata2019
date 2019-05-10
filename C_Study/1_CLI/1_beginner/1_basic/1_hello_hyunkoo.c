b7b5274a3eb12c5a8639d938d38d77e625ce7e21
Hyunkoo Lee <you@example.com>
2019-05-10 09:14:34 +0900

    5월 9일 신규파일

---
+++ C_Study/1_CLI/1_beginner/1_basic/1_hello_hyunkoo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main(){
+	printf("Hello Hyunkoo!");
+	printf("Hello World!");
+}
---
+++ C_Study/1_CLI/1_beginner/1_basic/2_hello_hyunkoo2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main(){
+	printf("Hello Hyunkoo!\n");
+	printf("Hello World!");
+}
