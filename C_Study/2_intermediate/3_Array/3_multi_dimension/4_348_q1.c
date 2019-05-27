8320aaa52e069b6166aea4204de1b52a5749a1bc
Hyunkoo Lee <you@example.com>
2019-05-27 09:13:39 +0900

    test

---
+++ C_Study/2_intermediate/3_Array/3_multi_dimension/4_348_q1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(){
+	int arr[3][9];
+	int i,j;
+
+	for(i=0;i<3;i++)
+		for(j=0;j<9;j++)
+			arr[i][j] = (i+2) * (j+1);
+
+	for(i=0;i<3;i++){
+		for(j=0;j<9;j++){
+			printf("%4d", arr[i][j]);
+		}
+		printf("\n");
+	}
+	
+}
